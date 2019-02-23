39bcb0ba4149018a60e0d0d91411c7c16a110a56
Sharmaine Requerme <srqrme@gmail.com>
2019-02-23 18:57:58 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -14,7 +14,7 @@ describe 'new post' do
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
    visit new_post_path
 
-   fill_in 'post_title', with: 'My post title'
+   fill_in 'post_title', with: "My post title"
    fill_in 'post_description', with: 'My post description'
 
    click_on 'Submit Post'
